a0a7addefa6dd019e8340c9f54c5f2b7cbf5e79b
Luka Hristic <luka.hristic@soprasteria.com>
2019-08-07 12:43:02 +0200

    Initial Commit

---
+++ Reflection/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reflection
+//
+//  Created by HRISTIC Luka on 07/08/2019.
+//  Copyright © 2019 Hristic Luka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
